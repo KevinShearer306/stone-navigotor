6c6a5bec4c3aaa53ac2b46ab7f7bbc00d23bacbc
KevinShearer306 <shearerag@outlook.com>
2025-05-23 22:14:51 -0600

    Add files via upload

---
+++ waypoints.js
@@ -0,0 +1,27 @@
+const waypoints = [
+  {
+    "lat": 51.098646,
+    "lon": -101.497208,
+    "name": "Stone AI"
+  },
+  {
+    "lat": 51.098715,
+    "lon": -101.498516,
+    "name": "Stone AI"
+  },
+  {
+    "lat": 51.107922,
+    "lon": -101.511064,
+    "name": "Stone AI"
+  },
+  {
+    "lat": 51.102793,
+    "lon": -101.497804,
+    "name": "Stone AI"
+  },
+  {
+    "lat": 51.02435,
+    "lon": -101.471888,
+    "name": "Stone_18"
+  }
+];
